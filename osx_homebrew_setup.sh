843490ae1b88b5f8661a804a4b6f0b7fac8d9ca6
Beau Graham <bgraham@lucidcahrt.com>
2019-02-06 16:20:04 -0700

    new way of installing homebrew without the password. I think.

--- osx_homebrew_setup.sh
+++ osx_homebrew_setup.sh
@@ -3,12 +3,48 @@
 
 
 # Checking for and installing Homebrew.
-if ! which brew > /dev/null; then
-      echo “Installing Homebrew...”
-      /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-  else
-    echo "Homebrew already installed."
-fi 
+#if ! which brew > /dev/null; then
+    #  echo “Installing Homebrew...”
+   #   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+  #else
+ #   echo "Homebrew already installed."
+#fi
+
+which -s brew
+if [[ $? = 1 ]];
+then
+    # Install Homebrew. This doesn't like being run as root so we must do this manually.
+    notify "Downloading Homebrew"
+
+    # Curl down the latest tarball and install to /usr/local
+    curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C /usr/local 2>&1 | tee -a ${LOG}
+
+    # Manually make all the appropriate directories and set permissions
+    mkdir -p /usr/local/Cellar /usr/local/Homebrew /usr/local/Frameworks /usr/local/bin /usr/local/etc /usr/local/include /usr/local/lib /usr/local/opt /usr/local/sbin /usr/local/share /usr/local/share/zsh /usr/local/share/zsh/site-functions /usr/local/var
+    chown -R $consoleuser /usr/local
+    chmod g+rwx /usr/local/Cellar /usr/local/Homebrew /usr/local/Frameworks /usr/local/bin /usr/local/etc /usr/local/include /usr/local/lib /usr/local/opt /usr/local/sbin /usr/local/share /usr/local/share/zsh /usr/local/share/zsh/site-functions /usr/local/var
+    chmod 755 /usr/local/share/zsh /usr/local/share/zsh/site-functions
+    chgrp admin /usr/local/Cellar /usr/local/Homebrew /usr/local/Frameworks /usr/local/bin /usr/local/etc /usr/local/include /usr/local/lib /usr/local/opt /usr/local/sbin /usr/local/share /usr/local/share/zsh /usr/local/share/zsh/site-functions /usr/local/var
+
+    # Create a system wide cache folder
+    mkdir -p /Library/Caches/Homebrew
+    chmod g+rwx /Library/Caches/Homebrew
+    chown $consoleuser /Library/Caches/Homebrew
+
+    # Remove temporary folder
+    rm -rf /usr/local/Homebrew
+
+    # Install the MD5 checker or the recipes will fail
+    notify "Installing md5sha1sum"
+    su -l "$consoleuser" -c "/usr/local/bin/brew install md5sha1sum" 2>&1 | tee -a ${LOG}
+    su -l "$consoleuser" -c "echo 'export PATH=/usr/local/opt/openssl/bin:\$PATH' >> /Users/$consoleuser/.bash_profile"
+else
+    # Run an update and quit
+    notify "Updating Homebrew"
+    su -l "$consoleuser" -c "/usr/local/bin/brew update" 2>&1 | tee -a ${LOG}
+    exit 0
+fi
+
 
 # Tapping casks for driver, versions and fonts
 /usr/local/bin/brew tap caskroom/drivers
